9750f3ec8f90a8bc96e9beb172ebfbd66dc89099
Viraj Gavade <vrajgavade17@gmail.com>
2024-07-04 13:23:14 +0530

    comment model schema

---
+++ Models/comment.models.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const mongooseAggregatePaginate = require('mongoose-aggregate-paginate-v2')
+
+const commentSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    video:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{
+    timestamps:true
+})
+
+commentSchema.plugin(mongooseAggregatePaginate)
+module.exports = mongoose.model('Comment'.commentSchema)
